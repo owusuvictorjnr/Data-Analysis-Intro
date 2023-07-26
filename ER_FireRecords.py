7ae91e645446f317f88e394a2e371d8e6960052f
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-26 05:50:29 -0400

    get info about the dataset

--- ER_FireRecords.py
+++ ER_FireRecords.py
@@ -4,4 +4,10 @@ import pandas as pd
 
 df = pd.read_csv('/home/vitech/Downloads/EAF_Report.csv', encoding='cp1252')
 
+# print number of rows
 # print (df.head(5))
+
+#get information about the dataset
+# print (df.info())
+
+
